e2981f7d932d0be6e7d70513263e34b02ec9ca91
Alistair <plantonly@gmail.com>
2014-09-24 15:09:23 -0700

    play tile is working and adjusted hover style for tiles

--- js/mvc.js
+++ js/mvc.js
@@ -13,15 +13,13 @@ function View(){
   this.tile3 = ".tile3"
   this.tile4 = ".tile4"
   this.round = ".round"
+  this.tile1Snd = new Audio("sounds/tile1.mp3")
+  this.tile2Snd = new Audio("sounds/tile2.mp3")
+  this.tile3Snd = new Audio("sounds/tile3.mp3")
+  this.tile4Snd = new Audio("sounds/tile4.mp3")
 }
 
 View.prototype = {
-  getAudio: function(){
-    this.tile1 = new Audio("sounds/tile1.mp3")
-    this.tile2 = new Audio("sounds/tile2.mp3")
-    this.tile3 = new Audio("sounds/tile3.mp3")
-    this.tile4 = new Audio("sounds/tile4.mp3")
-  },
   getStartButton: function(){
     return document.querySelector(this.startButton)
   },
@@ -45,8 +43,9 @@ Controller.prototype = {
     strBut.addEventListener('click', this.playTile.bind(this))
   },
   playTile: function(){
-    var play = this.game.playTiles(this.view.tile4)
-
+    console.log(this.game.randTile())
+    var rand = this.game.randTile()
+    this.game.playTiles(this.view["tile"+rand], this.view["tile"+rand+"Snd"])
   }
 }
 
@@ -59,8 +58,19 @@ Game.prototype = {
   randTile: function(){
     return this.randNum = (Math.floor(Math.random()*4)+1)
   },
-  playTiles: function(tile){
-     document.querySelector(tile).style.backgroundColor += "#444"
-
+  playTiles: function(tile, snd){
+    document.querySelector(tile).style.opacity = "1"
+    tile
+    snd.play()
+    this.timer(
+      function(){
+        document.querySelector(tile).style.opacity = ".8"
+        snd.pause()
+        snd.currentTime = 0
+      },470
+    )
+  },
+  timer: function(func, amount){
+    var timeFunc = setTimeout(func, amount)
   }
 }
